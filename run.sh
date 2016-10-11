800ac639d8004005fc275478d4c5fe26f5ad69aa
Olle Hallin <olle.hallin@hit.se>
2016-10-11 12:17:19 +0200

    Made dconf editing work. Simplified tags

--- run.sh
+++ run.sh
@@ -10,3 +10,7 @@ fi
 
 cd $(dirname $0)/ansible
 sudo ansible-playbook -i inventory playbook.yml -e "actual_username=${USER}" $*
+
+# These are hard to set via Ansible. Do it in a simpler way...
+dconf write /org/gnome/settings-daemon/plugins/media-keys/screensaver "'<Ctrl>section'"
+dconf write /org/gnome/desktop/wm/preferences/mouse-button-modifier "'<Alt><Super>'"
