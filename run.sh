4dbb8fdb4ab880e4069745d7a49f43e07842d875
Olle Hallin <olle.hallin@hit.se>
2016-11-09 10:02:16 +0100

    Moved customer specific stuff to customer1.yml

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ if [ "$ansible" == "" -o "$ohai" == "" ]; then
 fi
 
 cd $(dirname $0)/ansible
-sudo ansible-playbook --vault-password-file=$HOME/.ansible-vault-password -i inventory playbook.yml -e "actual_username=${USER}" $*
+sudo ansible-playbook --vault-password-file=$HOME/.ansible-vault-password -i inventory ${PLAYBOOK:-playbook.yml} -e "actual_username=${USER}" $*
 
 # These are hard to set via Ansible. Do it in a simpler way...
 dconf write /org/gnome/settings-daemon/plugins/media-keys/screensaver "'<Ctrl>section'"
