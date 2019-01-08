26991614452a0fa2fac52b6877ec197af1e953b8
Olle Hallin <olle.hallin@crisp.se>
2019-01-08 11:34:03 +0100

    Bugfix in sdk.fish

--- ansible/files/fish/functions/sdk.fish
+++ ansible/files/fish/functions/sdk.fish
@@ -1,43 +1,41 @@
 # Source: http://tedwise.com/2016/02/26/using-sdkman-with-the-fish-shell
 
 function sdk --description 'Software Development Kit Manager'
-  set after_env (mktemp -t after_env.XXXXXX)
-  set path_env (mktemp -t path_env.XXXXXX)
+  set after_env (mktemp -t env.XXXXXX)
+  set path_env (mktemp -t env.XXXXXX)
 
-    bash -c "source ~/.sdkman/bin/sdkman-init.sh && printenv > $after_env"
+  bash -c "source ~/.sdkman/bin/sdkman-init.sh && sdk $argv && printenv > $after_env"
 
-    # remove any pre-existing .sdkman paths
-    for elem in $PATH
-      switch $elem
-        case '*/.sdkman/*'
-          # ignore
-        case '*'
-          echo "$elem" >> $path_env
-      end
+  # remove any pre-existing .sdkman paths
+  for elem in $PATH
+    switch $elem
+      case '*/.sdkman/*'
+        # ignore
+      case '*'
+        echo "$elem" >> $path_env
     end
+  end
 
-    for env in (cat $after_env)
-      set env_name (echo $env | sed s/=.\*//)
-      set env_value (echo $env | sed s/.\*=//)
-      switch $env_name
-        case 'PATH'
-          for elem in (echo $env_value | tr ':' '\n')
-            switch $elem
-              case '*/.sdkman/*'
-                echo "$elem" >> $path_env
-            end
-          end
-        case '*'
-          switch $env_value
+  for env in (cat $after_env)
+    set env_name (echo $env | sed s/=.\*//)
+    set env_value (echo $env | sed s/.\*=//)
+    switch $env_name
+      case 'PATH'
+        for elem in (echo $env_value | tr ':' '\n')
+          switch $elem
             case '*/.sdkman/*'
-              set -ge $env_name
-              eval set -U $env_name $env_value > /dev/null
+              echo "$elem" >> $path_env
           end
-      end
+        end
+      case '*'
+        switch $env_value
+          case '*/.sdkman/*'
+            eval set -Ux $env_name $env_value > /dev/null
+        end
     end
-    set -ge PATH
-    set -Ux PATH (cat $path_env) ^ /dev/null
+  end
+  set -Ux PATH (cat $path_env) ^ /dev/null
 
-    rm -f $after_env
-    rm -f $path_env
+  rm -f $after_env
+  rm -f $path_env
 end
