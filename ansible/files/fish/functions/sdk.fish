c1d6cf38bd199a02b12bb9eb0cda7f76ea9c0362
Olle Hallin <olle.hallin@hit.se>
2019-01-11 11:43:32 +0100

    Fixed PATH ordering problem

--- ansible/files/fish/functions/sdk.fish
+++ ansible/files/fish/functions/sdk.fish
@@ -6,7 +6,28 @@ function sdk --description 'Software Development Kit Manager'
 
   bash -c "source ~/.sdkman/bin/sdkman-init.sh && sdk $argv && printenv > $after_env"
 
-  # remove any pre-existing .sdkman paths
+  for env in (cat $after_env)
+    set env_name (echo $env | sed s/=.\*//)
+    set env_value (echo $env | sed s/.\*=//)
+    switch $env_name
+      case 'PATH'
+        for elem in (echo $env_value | tr ':' '\n')
+          switch $elem
+            case '*/.sdkman/*'
+              # Make sure .sdkman paths appear first in resulting PATH
+              echo "$elem" >> $path_env
+          end
+        end
+      case '*'
+        switch $env_value
+          case '*/.sdkman/*'
+            # Probably a XXX_HOME variable...
+            eval set -gx $env_name $env_value > /dev/null
+        end
+    end
+  end
+
+  # Append non-.sdkman locations to PATH...
   for elem in $PATH
     switch $elem
       case '*/.sdkman/*'
@@ -16,25 +37,7 @@ function sdk --description 'Software Development Kit Manager'
     end
   end
 
-  for env in (cat $after_env)
-    set env_name (echo $env | sed s/=.\*//)
-    set env_value (echo $env | sed s/.\*=//)
-    switch $env_name
-      case 'PATH'
-        for elem in (echo $env_value | tr ':' '\n')
-          switch $elem
-            case '*/.sdkman/*'
-              echo "$elem" >> $path_env
-          end
-        end
-      case '*'
-        switch $env_value
-          case '*/.sdkman/*'
-            eval set -Ux $env_name $env_value > /dev/null
-        end
-    end
-  end
-  set -Ux PATH (cat $path_env) ^ /dev/null
+  set -gx PATH (cat $path_env) ^ /dev/null
 
   rm -f $after_env
   rm -f $path_env
