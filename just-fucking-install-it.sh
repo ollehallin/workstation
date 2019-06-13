91452e758bcfbeb3e986179cb3465a7882d14070
Olle Hallin <olle.hallin@crisp.se>
2019-06-13 10:51:10 +0200

    shellchecked just-fucking-install-it.sh

--- just-fucking-install-it.sh
+++ just-fucking-install-it.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-if [ "$(which ansible)" == "/usr/local/bin/ansible" ]; then
+if [ "$(command -v ansible)" == "/usr/local/bin/ansible" ]; then
     echo "Removing pip-installed ansible:"
     sudo pip uninstall ansible
 fi
 
-if [ "$(grep ppa.launchpad.net/ansible/ansible /etc/apt/sources.list.d/ansible*.list)" == "" -o  "$(which ohai)" == "" ]; then
+if [ "$(grep ppa.launchpad.net/ansible/ansible /etc/apt/sources.list.d/ansible*.list | grep -Ev '#\s*deb')" == "" ] || [ "$(command -v ohai)" == "" ]; then
     echo "Adding APT key for ppa:ansible/ansible:"
     sudo apt-add-repository -y ppa:ansible/ansible
     sudo apt update
     sudo apt install -y software-properties-common ansible ohai libssl-dev
 fi
 
-if [ "$(which git-crypt)" == "" ]; then
+if [ "$(command -v git-crypt)" == "" ]; then
     echo "Installing git-crypt..."
     sudo apt install git-crypt
 
@@ -20,12 +20,12 @@ if [ "$(which git-crypt)" == "" ]; then
     git-crypt unlock
 fi
 
-cd $(dirname $0)/ansible
+cd "$(dirname "$0")/ansible" || exit
 touch ~/.netrc
 sudo chown root.root ~/.netrc
-sudo ansible-playbook -i inventory ${PLAYBOOK:-playbook.yml} -e "pwd=${PWD}" -e "actual_username=${USER}" $*
-sudo chown ${USER}.${USER} ~/.netrc
-sudo chown -R ${USER}.${USER} ~/.cache
+sudo ansible-playbook -i inventory "${PLAYBOOK:-playbook.yml}" -e "pwd=${PWD}" -e "actual_username=${USER}" $*
+sudo chown "${USER}"."${USER}" ~/.netrc
+sudo chown -R "${USER}"."${USER}" ~/.cache
 
 # These are hard to set via Ansible. Do it in a simpler way...
 # gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "'<Ctrl>section'"
