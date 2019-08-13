5dd40094de60fc37f68ab2d93ced348b5b2734bc
Olle Hallin <olle.hallin@hit.se>
2019-08-13 10:42:39 +0200

    Added AWS MFA token script for customer4

---
+++ ansible/templates/customer4/sonic-token.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+cd $(find ~/work -type d -name fr-start)
+npm run aws:mfa arn:aws:iam::{{ secrets.aws.customer4.account }}:mfa/olle_hallin@{{ secrets.customer4.name }}.com $1 {{ secrets.customer4.name }}
