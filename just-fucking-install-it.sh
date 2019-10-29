d3e2e97fd7613b29d460d57623a87988c417ed0a
Olle Hallin <olle.hallin@crisp.se>
2019-10-29 17:19:41 +0100

    Fixed after upgrading to Ubuntu 19.10

--- just-fucking-install-it.sh
+++ just-fucking-install-it.sh
@@ -3,15 +3,19 @@
 if [[ "$(command -v ansible)" == "/usr/bin/ansible" ]]; then
     echo "Removing apt-installed ansible:"
     sudo apt remove ansible
-
+fi
+if [[ "$(command -v pip3)" == "" ]]; then
     echo "Installing python3-pip:"
     sudo apt install python3-pip
+fi
 
+if [[ "$(command -v ansible)" == "" ]]; then
     echo "Installing Ansible with pip3:"
     sudo pip3 install ansible
 fi
 
 if [[ "$(command -v ohai)" == "" ]]; then
+    echo "Installing ohai"
     sudo apt install -y software-properties-common ohai libssl-dev
 fi
 
@@ -24,11 +28,11 @@ if [[ "$(command -v git-crypt)" == "" ]]; then
 fi
 
 cd "$(dirname "$0")/ansible" || exit
-touch ~/.netrc
+trap 'sudo chown -R "${USER}"."${USER}" ~/.netrc ~/.cache' EXIT
+sudo touch ~/.netrc
 sudo chown root.root ~/.netrc
+
 sudo ansible-playbook -i inventory "${PLAYBOOK:-playbook.yml}" -e "pwd=${PWD}" -e "actual_home=${HOME}" -e "actual_username=${USER}" $*
-sudo chown "${USER}"."${USER}" ~/.netrc
-sudo chown -R "${USER}"."${USER}" ~/.cache
 
 # These are hard to set via Ansible. Do it in a simpler way...
 # gsettings set org.gnome.settings-daemon.plugins.media-keys screensaver "'<Ctrl>section'"
